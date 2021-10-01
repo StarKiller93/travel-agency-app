3f44055cc1d18afa68595a373c4151051d130609
StarKiller93 <na061493@gmail.com>
2021-10-01 04:52:42 +0300

    Initial commit

---
+++ app/js/script.js
@@ -0,0 +1,29 @@
+// Sticky Navigation
+const sectionHeroEl = document.querySelector(".hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
